713fa66a8053a40a865509e2b7d2935143373a63
Ivo Cazemier <git@warmemelk.nl>
2022-08-31 15:34:14 +0200

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// Streams API some possibilities
+
+/**
+ *   @example
+ *   ```
+ *   # Planning
+ * 
+ *   ## Stream:
+ *   1. Data => output
+ *   1. Data => Transform data => local file CSV
+ *   1. From MongoDB => Transform data => CSV local file
+ *   1. From MongoDB => Transform data => CSV => local compressed (zip) file
+ *   1. From MongoDB => Transform data => CSV => compress (zip) => S3
+ *   1. From SQL => Transform data => CSV => compress (zip) => S3
+ * 
+ *   ## How to: Unit test a stream
+ * 
+ * 
+ *   
+ *
+ *   ```
+ */
+
+
+console.log('Dope!');
